2bce86b11d34def272a5d62db6eb990771f040ba
ruandias <ruan_ndias@hotmail.com>
2021-03-03 22:45:19 -0300

    add HTTP Methods

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -32,8 +32,59 @@ namespace Catalog.Controllers
             var item = _repository.GetItem(id);
             if (item == null)
                 return NotFound();
-            
+
             return item.AsDto();
+        }
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            _repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = _repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item updatedItem = existingItem with //with expressions
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            _repository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = _repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            _repository.DeleteItem(id);
+
+            return NoContent();
         }
     }
 }
